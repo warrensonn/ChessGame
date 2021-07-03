51290ae08548a5cd126bef1a187a03dbd5165721
Warren <bevilacqua.warren@gmail.com>
2021-07-03 12:31:51 +0200

    No longer contains every type of pieces classes, only King class. Added King's movements

--- Pieces/king.py
+++ Pieces/king.py
@@ -1,41 +1,82 @@
-import pygame
 from Pieces.piece import Piece
+
 
 class King(Piece):
 
-    def __init__(self, game, image, color, position):  # x et y doivent devenir Coordonnées
-        super().__init__(game, image, color, position)
+    def __init__(self, game, image, color, position, name):
+        super().__init__(game, image, color, position, name)
         self.game.all_Pieces.add(self)
 
-    def move(self):
-        return super().move()
+    def possibleMoves(self):
+        moves = []
+        empty = True
         
-class Queen(Piece):
+        # LES 3 DEPLACEMENTS VERS LE HAUT 
+        if self.intPosition[1] < 8: 
+            for piece in self.game.all_Pieces:
+                if piece.intPosition[0] == self.intPosition[0] and piece.intPosition[1] == self.intPosition[1]+1 and piece.team == self.team:
+                    empty = False
+            if empty == True:
+                moves.append((self.intPosition[0], self.intPosition[1]+1))
 
-    def __init__(self, game, image, color, position):
-        super().__init__(game, image, color, position)
-        self.game.all_Pieces.add(self)
-        
-class Rook(Piece): # Tour
+        empty= True
+        if self.intPosition[1] < 8 and self.intPosition[0] > 1:
+            for piece in self.game.all_Pieces:
+                if piece.intPosition[0] == self.intPosition[0]-1 and piece.intPosition[1] == self.intPosition[1]+1 and piece.team == self.team:
+                    empty = False
+            if empty == True:
+                moves.append((self.intPosition[0]-1, self.intPosition[1]+1))
 
-    def __init__(self, game, image, color, position):
-        super().__init__(game, image, color, position)
-        self.game.all_Pieces.add(self)
+        empty= True
+        if self.intPosition[1] < 8 and self.intPosition[0] < 8:
+            for piece in self.game.all_Pieces:
+                if piece.intPosition[0] == self.intPosition[0]+1 and piece.intPosition[1] == self.intPosition[1]+1 and piece.team == self.team:
+                    empty = False
+            if empty == True:
+                moves.append((self.intPosition[0]+1, self.intPosition[1]+1))
 
-class Bishop(Piece): # Fou
 
-    def __init__(self, game, image, color, position):
-        super().__init__(game, image, color, position)
-        self.game.all_Pieces.add(self)
+        # LES 3 DEPLACEMENTS VERS LE BAS 
+        empty= True
+        if self.intPosition[1] > 1 and self.intPosition[0] < 8:
+            for piece in self.game.all_Pieces:
+                if piece.intPosition[0] == self.intPosition[0]+1 and piece.intPosition[1] == self.intPosition[1]-1 and piece.team == self.team:
+                    empty = False
+            if empty == True:
+                moves.append((self.intPosition[0]+1, self.intPosition[1]-1))
 
-class Knight(Piece):
+        empty= True
+        if self.intPosition[1] > 1 and self.intPosition[0] > 1:
+            for piece in self.game.all_Pieces:
+                if piece.intPosition[0] == self.intPosition[0]-1 and piece.intPosition[1] == self.intPosition[1]-1 and piece.team == self.team:
+                    empty = False
+            if empty == True:
+                moves.append((self.intPosition[0]-1, self.intPosition[1]-1))
 
-    def __init__(self, game, image, color, position):
-        super().__init__(game, image, color, position)
-        self.game.all_Pieces.add(self)
+        empty= True
+        if self.intPosition[1] > 1:
+            for piece in self.game.all_Pieces:
+                if piece.intPosition[0] == self.intPosition[0] and piece.intPosition[1] == self.intPosition[1]-1 and piece.team == self.team:
+                    empty = False
+            if empty == True:
+                moves.append((self.intPosition[0], self.intPosition[1]-1))
 
-class Pawn(Piece):
 
-    def __init__(self, game, image, color, position):
-        super().__init__(game, image, color, position)
-        self.game.all_Pieces.add(self)
+        # LES 2 DEPLACEMENTS GAUCHE ET DROITE 
+        empty= True
+        if self.intPosition[0] > 1:
+            for piece in self.game.all_Pieces:
+                if piece.intPosition[0] == self.intPosition[0]-1 and piece.intPosition[1] == self.intPosition[1] and piece.team == self.team:
+                    empty = False
+            if empty == True:
+                moves.append((self.intPosition[0]-1, self.intPosition[1]))
+
+        empty= True
+        if self.intPosition[0] < 8:
+            for piece in self.game.all_Pieces:
+                if piece.intPosition[0] == self.intPosition[0]+1 and piece.intPosition[1] == self.intPosition[1] and piece.team == self.team:
+                    empty = False
+            if empty == True:
+                moves.append((self.intPosition[0]+1, self.intPosition[1]))
+
+        return moves
