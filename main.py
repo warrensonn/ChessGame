cb97257b58c0794382b5f5e879772f104f7d06ec
Warren <bevilacqua.warren@gmail.com>
2021-07-07 22:29:39 +0200

    Using the new moving function from Game.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from Pieces.king import King
+from Pieces.rook import Rook
 from game import Game
 import pygame
 from Pieces.piece import Piece
@@ -6,16 +8,16 @@ pygame.init()
 game = Game()
 
 
-pygame.display.set_caption('Bevilacqua Warren Chess Game')
+pygame.display.set_caption('Bevilacqua Warren Chess Game v1.0')
 screen = pygame.display.set_mode((1080, 720))
 
-BLACK = pygame.Color(0,0,0)
+BLUE = pygame.Color(10,10,100)
 
 running = True
 
 while running: 
     
-    screen.fill(BLACK)         # on injecte l'image sur l'écran  
+    screen.fill(BLUE)   # on injecte l'image sur l'écran  
 
     game.update(screen)
     
@@ -30,21 +32,21 @@ while running:
 
 
         elif event.type == pygame.MOUSEBUTTONUP:
-            if isinstance(game.selectedBox, Piece):
+            if isinstance(game.selectedBox, Piece): # Si une pièce est selectionnée
 
                 for area in game.board.areas:   # Mettre en fonction
                     if game.board.areas.get(area)[0][0] <= event.pos[0] <= game.board.areas.get(area)[0][1] and game.board.areas.get(area)[1][0] <= event.pos[1] <= game.board.areas.get(area)[1][1]:   
                         selectedArea = area  # = à un nom de case style a1 a2 etc
                 
                 if game.selectedBox.isPossibleMove(selectedArea) == True:
-                    game.selectedBox.position = selectedArea
-                    game.selectedBox = False 
+                    game.moving(selectedArea)
                     print("Au tour des", game.turn)
                 else:
                     print(game.selectedBox.isPossibleMove(selectedArea))
                     game.selectedBox = False
 
-            else:
+            else:  # Si aucune pièce n'est sélectionnée
+
                 for area in game.board.areas:
                     if game.board.areas.get(area)[0][0] <= event.pos[0] <= game.board.areas.get(area)[0][1] and game.board.areas.get(area)[1][0] <= event.pos[1] <= game.board.areas.get(area)[1][1]:   
                         selectedArea = area
